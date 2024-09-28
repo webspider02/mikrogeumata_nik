bea642304add571512023943ffe4c1895389e564
webspider02 <giorgoszampakikas@gmail.com>
2024-09-28 13:06:25 +0300

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import '../styles/Contact.css';
 
 export default function Contact() {
     return (
-        <div className="contact">
-            <h3>Επικοινωνία</h3>
-            <p>Τηλ. 2441040660</p>
-            <p>Κινητ. 6985981047</p>
-            <p>Διευθυνση: Αργοναύτων 1, Καρδίτσα 43100</p>
+        <div className="page-container">
+            <div className="contact">
+                <h3>Επικοινωνία</h3>
+                <p>Τηλ. 2441040660</p>
+                <p>Κινητ. 6985981047</p>
+                <p>Διευθυνση: Αργοναύτων 1, Καρδίτσα 43100</p>
+            </div>            
         </div>
+
     )
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,12 +3,120 @@ import react from 'react';
 
 export default function Menu() {
     return (
-        <div className="menu">
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
+        <div className="page-container">
+            <div className="menu">
+                
+                <ul>
+                    <h3>Τυλιχτά με πίτα</h3>
+                    <li>
+                        <p>Γύρος χοιρινός: 3,30€</p>
+                        <p>(τζατζίκι, ντομάτα, κρεμμύδι, μουστάρδα, πατάτες)</p>
+                    </li>
+                    <li>
+                        <p>Σουβλάκι χοιρινό: 3,00€</p>
+                        <p>(τζατζίκι, ντομάτα, κρεμμύδι, μουστάρδα, πατάτες)</p>
+                    </li>
+                    <li>
+                        <p>Μπιφτέκι χειροποίητος: 3,30€</p>
+                        <p>(τζατζίκι, ντομάτα, κρεμμύδι, μουστάρδα, πατάτες)</p>
+                    </li>
+                    <li>
+                        <p>Γύρος κοτόπουλο: 3,30€</p>
+                        <p>(μαγιονέζα ή σως μουστάρδας, ντομάτα, πατάτες)</p>
+                    </li>
+                    <li>
+                        <p>Σουβλάκι κοτόπουλο: 3,00€</p>
+                        <p>(μαγιονέζα ή σως μουστάρδας, ντομάτα, πατάτες)</p>
+                    </li>
+                    <li>
+                        <p>Κοτομπουκίες: 3,30€</p>
+                        <p>(σως μουστάρδας, ντομάτα, πατάτες)</p>
+                    </li>
+                    <li>
+                        <p>Φιλέτο κοτόπουλο: 3,30€</p>
+                        <p>(σως μουστάρδας, ντομάτα, πατάτες)</p>
+                    </li>
+                </ul>
+                
+                <ul>
+                    <h3>Σάντουιτς ψωμάκι</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Μπαγκέτα</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Σάντουιτς με Κυπριακή</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Σκεπαστή</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Burger</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Club Sandwich</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Μερίδες</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Σαλάτες</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Ορεκτικά</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Αλοιφές</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Τεμάχια</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Αναψυκτικά</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>                
+                <ul>
+                    <h3>Χυμοί Μπύρες Κρασί</h3>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </div>
         </div>
     );
 }
