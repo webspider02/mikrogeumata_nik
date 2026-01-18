2be0a7de2338632f9b646e448f62b5a45a8ae707
webspider02 <giorgoszampakikas@gmail.com>
2026-01-18 12:11:16 +0200

    feat: responsive

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,3 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import '../styles/Home.css';
-
-// export default function Home() {
-//     return (
-//         <div className="page-container">
-//             <div className="home">
-//                 <div className="img-cards">
-//                     <img src={process.env.PUBLIC_URL + '/images/magazi.jpg'} alt="magazi.png" />
-//                 </div>
-//                 <div className="img-cards">
-//                     <img src={process.env.PUBLIC_URL + '/images/gyros.jpg'} alt="mikrogeumata.png" />
-//                 </div>
-//                 <div id="home-button">
-//                     <Link to="/menu">
-//                         <button>Κατάλογος</button>
-//                     </Link>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Home.css';
