574cffa05bcdcb5669f494da60789f2630a8d9da
webspider02 <giorgoszampakikas@gmail.com>
2026-01-18 11:59:46 +0200

    feat: css rework

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,27 @@
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import '../styles/Home.css';
+
+// export default function Home() {
+//     return (
+//         <div className="page-container">
+//             <div className="home">
+//                 <div className="img-cards">
+//                     <img src={process.env.PUBLIC_URL + '/images/magazi.jpg'} alt="magazi.png" />
+//                 </div>
+//                 <div className="img-cards">
+//                     <img src={process.env.PUBLIC_URL + '/images/gyros.jpg'} alt="mikrogeumata.png" />
+//                 </div>
+//                 <div id="home-button">
+//                     <Link to="/menu">
+//                         <button>Κατάλογος</button>
+//                     </Link>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Home.css';
@@ -6,18 +30,30 @@ export default function Home() {
     return (
         <div className="page-container">
             <div className="home">
-                <div className="img-cards">
-                    <img src={process.env.PUBLIC_URL + '/images/magazi.jpg'} alt="magazi.png" />
+                
+                <div className="images-wrapper">
+                    <div className="img-cards">
+                        <img
+                            src={process.env.PUBLIC_URL + '/images/magazi.jpg'}
+                            alt="Μαγαζί"
+                        />
+                    </div>
+
+                    <div className="img-cards">
+                        <img
+                            src={process.env.PUBLIC_URL + '/images/gyros.jpg'}
+                            alt="Γύρος"
+                        />
+                    </div>
                 </div>
-                <div className="img-cards">
-                    <img src={process.env.PUBLIC_URL + '/images/gyros.jpg'} alt="mikrogeumata.png" />
-                </div>
+
                 <div id="home-button">
                     <Link to="/menu">
                         <button>Κατάλογος</button>
                     </Link>
                 </div>
+
             </div>
         </div>
-    )
+    );
 }
